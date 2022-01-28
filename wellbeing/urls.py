af34c5af546281ef61ba2ef015001cddc583818b
Joanna Gorska <gorska919@gmail.com>
2022-01-28 23:11:38 +0000

    Fix PostMood class view and redirect to home

--- wellbeing/urls.py
+++ wellbeing/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('', views.PostMood.as_view(), name='post_mood'),
+    path('wellbeing/<int:author_id>', views.PostMood.as_view(), name='post_mood'),
 ]
--- wellbeing/views.py
+++ wellbeing/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import CreateView
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from .models import Mood
 from .forms import MoodForm
 
@@ -25,13 +26,17 @@ class PostMood(CreateView):
     '''
     
     '''
+    template_name = 'index.html'
+    form_class = MoodForm
+    success_url = 'home'
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request, author_id, *args, **kwargs):
         mood_form = MoodForm(data=request.POST)
+
         if mood_form.is_valid():
-            mood_form.instance.author = request.user.id
-            mood_instance = mood_form.save(comit=False)
-            mood_instance.save()
+            author = get_object_or_404(User, id=author_id)
+            mood_form.instance.author = author
+            mood_instance = mood_form.save()
         else:
             mood_form = MoodForm()
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect("/")
