126e45b22ad422681f87f1852d4c0cc4aa68f2f5
Joanna Gorska <gorska919@gmail.com>
2022-01-29 13:27:52 +0000

    Add urls to handle ClickMood view

--- wellbeing/urls.py
+++ wellbeing/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('wellbeing/<int:author_id>', views.PostMood.as_view(), name='post_mood'),
+    path('wellbeing/<int:author_id>/<str:mood>', views.ClickMood.as_view(), name='click_mood'),
+
 ]
