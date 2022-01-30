6edb39844efe355e5c277cb7527d4b1c08f84bf7
Joanna Gorska <gorska919@gmail.com>
2022-01-30 19:17:20 +0000

    Add views in jocking app

--- jocking/views.py
+++ jocking/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
+from .models import Jocke
+from .forms import JockeForm
 
-# Create your views here.
+class JockesList(generic.ListView):
+    '''
+    class view enabling to display the list of jockes with pagination
+    '''
+    model = Jocke
+    queryset = Jocke.objects.order_by('-created_on')
+    template_name = 'jockes_list_paginated.html'
+    paginate_by = 6
+
+
+class AddNewJocke(CreateView):
+    '''
+    class view in get - gets the jocke_form and in post - posts the form
+    and creates new jocke
+    '''
+    template_name = 'jocke_form.html'
+    form_class = JockeForm
+    success_url = 'jockes_list'
+
+    def get(self, request, *args, **kwargs):
+
+        return render(
+            request,
+            'jocke_form.html',
+            {
+                'jocke_form': JockeForm()
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+
+        jocke_form = JockeForm(data=request.POST)
+        if jocke_form.is_valid():
+            jocke_form.instance.author_id = request.user.id
+
+            jocke = jocke_form.save(commit=False)
+            jocke.save()
+
+        else:
+            jocke_form = JockeForm()
+
+        return render(request, 'jockes_list_paginated.html')
+
+
+class JockeEyes(View):
+    '''
+    adds user to the users list that have clicked the eye
+    '''
+    def post(self, request, msg_id):
+        jocke = get_object_or_404(Jocke, id=jocke_id)
+        if jocke.eyes.filter(id=request.user.id).exists():
+            jocke.eyes.remove(request.user)
+        else:
+            jocke.eyes.add(request.user)
+
+        return render(request, 'jockes_list_paginated.html')
