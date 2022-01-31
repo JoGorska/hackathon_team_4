681158b819d16eaf6d18afed59b6418ded2284ae
Joanna Gorska <gorska919@gmail.com>
2022-01-31 13:15:47 +0000

    Fix cloudinary setting

--- self_care/settings.py
+++ self_care/settings.py
@@ -161,7 +161,7 @@ CLOUDINARY_STORAGE = {
 }
 
 
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
