24748868ae108a9381bd952c315a5a341be1e4fd
Joanna Gorska <gorska919@gmail.com>
2022-01-31 13:08:16 +0000

    fix cloudinary settings

--- self_care/settings.py
+++ self_care/settings.py
@@ -154,11 +154,11 @@ DEFAULT_FILE_STROAGE = 'cloudinary_sorage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 
-CLOUDINARY_STORAGE = {
+CLOUDINARY_STORAGE = [
     "CLOUD_NAME": os.environ.get('CLOUD_NAME'),
     "API_KEY": os.environ.get('API_KEY'),
     "API_SECRET": os.environ.get('API_SECRET')
-}
+]
 
 
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
