23fc68b000637468b1824a1ea848626807ee6b68
Joanna Gorska <gorska919@gmail.com>
2022-04-07 07:53:39 +0000

    Removed unused url

--- wellbeing/urls.py
+++ wellbeing/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('wellbeing/<int:author_id>', views.PostMood.as_view(), name='post_mood'),
     path('wellbeing/<int:author_id>/<str:mood>', views.ClickMood.as_view(), name='click_mood'),
     path('tired', views.get_tired_page, name='tired'),
     path('bored', views.get_bored_page, name='bored'),
